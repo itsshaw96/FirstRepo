37b4c653386f146e3bf5d8225fe468a949f58488
Shawvik <shawviksarkar@gmail.com>
2024-05-08 12:06:08 +0530

    Changes fot terry

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -108,7 +108,7 @@ public class CruddemoApplication {
 	private void createMultipleStudents(StudentDAO studentDAO) {
 		System.out.println("Creating 3 student objects ...");
 		Student tempStudent1 = new Student("Jenny","Doe","john@luv2code.com");
-		Student tempStudent2 = new Student("Mary","Public","mary@luv2code.com");
+		Student tempStudent2 = new Student("Terry","Public","mary@luv2code.com");
 		Student tempStudent3 = new Student("Bonita","Applebum","bonita@luv2code.com");
 		System.out.println("Saving the students ...");
 		studentDAO.save(tempStudent1);
